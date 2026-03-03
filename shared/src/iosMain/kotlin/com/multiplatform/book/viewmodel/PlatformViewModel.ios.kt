f9cf09e1737a257b9e590a7584a80c47b1fba905
fluttertutorialin <kamal.lakhani56@gmail.com>
2026-03-03 12:28:13 +0530

    ios lazy CoroutineScope

--- shared/src/iosMain/kotlin/com/multiplatform/book/viewmodel/PlatformViewModel.ios.kt
+++ shared/src/iosMain/kotlin/com/multiplatform/book/viewmodel/PlatformViewModel.ios.kt
@@ -9,8 +9,9 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-actual open class PlatformViewModel {
+/*actual open class PlatformViewModel {
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
 
     protected actual val platformViewModelScope: CoroutineScope
@@ -27,11 +28,50 @@ actual open class PlatformViewModel {
             }
         }
     }
+}*/
+
+
+actual open class PlatformViewModel {
+
+    private val scope: CoroutineScope by lazy(::buildScope)
+
+    protected actual val platformViewModelScope: CoroutineScope
+        get() = scope
+
+    private fun buildScope(): CloseableCoroutineScope = CloseableCoroutineScope(
+            SupervisorJob() + Dispatchers.Main.immediate
+        ).apply {
+            close()
+    }
+
+    protected actual open fun clear() {
+        scope.cancel()
+    }
+
+    fun observeFLow(flow: StateFlow<*>, onChange: (Any?) -> Unit) {
+        platformViewModelScope.launch {
+            flow.collect { value ->
+                onChange(value)
+            }
+        }
+    }
 }
 
 object ViewModelFactory {
     fun booksViewModel(): BooksViewModel {
         return BooksViewModel(createBookRepository())
+    }
+}
+
+class CloseableCoroutineScope(
+    context: CoroutineContext
+) : CoroutineScope {
+
+    private val job = Job(context[Job])
+    override val coroutineContext: CoroutineContext = context + job
+
+    fun close() {
+        coroutineContext.cancel()
     }
 }
 
